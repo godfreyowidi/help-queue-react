61e9c08f6c8f60101770363eeee7c86888da17d0
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-09-05 09:19:13 +0300

    add click handler to jsx

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -6,13 +6,28 @@ class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisiblePage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({formVisibleOnPage : true});
   }
 
   render() {
+    let currentlyVisibleState = null;
+    let addTicketButton = null;
+    if (this.state.formVisiblePage) {
+      currentlyVisibleState = <NewTicketForm />
+    } else {
+      currentlyVisibleState = <TicketList />
+      addTicketButton = <button onClick={this.handleClick}>Add Tickets</button>
+    }
     return (
       <React.Fragment>
-
+        {currentlyVisibleState}
+        {addTicketButton}
       </React.Fragment>
     );
   }
